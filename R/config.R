32652b9d517c213a0dc62468c40ff15a25c3668c
Kaiyu-W <79740043+Kaiyu-W@users.noreply.github.com>
2022-01-18 23:16:09 +0800

    Update config.R

--- R/config.R
+++ R/config.R
@@ -1,11 +1,18 @@
 #' to ignore the warning and error of correlation computation
 #'
-#' @param ... parameters of function cor
+#' @param x x
+#' @param y y
+#' @param ... other parameters of function cor
 #'
 #' @export
 #'
-mycor <- function(...) {
-    obj <- suppressWarnings(stats::cor(...))
+mycor <- function(x, y = NULL, ...) {
+    if (is(x, "dgCMatrix"))
+        x <- as.matrix(x)
+    if (is(y, "dgCMatrix"))
+        y <- as.matrix(y)
+    
+    obj <- suppressWarnings(stats::cor(x = x, y = y, ...))
     obj[is.na(obj)] <- 1
     obj
 }
